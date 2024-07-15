cd9247737d98963f7a4a460fb2c5b57348e17367
Suryakant01 <suryakant160502@gmail.com>
2024-07-15 19:58:37 +0530

    addding google auth and email and pass auth fn

--- src/context/firebase.context.jsx
+++ src/context/firebase.context.jsx
@@ -2,6 +2,17 @@ import { createContext, useContext } from "react";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+  sendSignInLinkToEmail,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
 const FirebaseContext = createContext(null);
 
 const firebaseConfig = {
@@ -11,26 +22,29 @@ const firebaseConfig = {
   storageBucket: "safarnama-c075f.appspot.com",
   messagingSenderId: "813604453737",
   appId: "1:813604453737:web:1feb3dc008d28e52c0d1f8",
-  measurementId: "G-Y27FSW4K5D"
+  measurementId: "G-Y27FSW4K5D",
 };
-
 
 const firebaseApp = initializeApp(firebaseConfig);
 const analytics = getAnalytics(FirebaseContext);
-
+const FirebaseAuth = getAuth();
+const googleAuth = new GoogleAuthProvider();
 
 export const useFirebase = () => useContext(FirebaseContext);
 
-
 export const FirebaseProvider = (props) => {
 
+  const signInWithGoogle = () => signInWithPopup(FirebaseAuth, googleAuth);
+
+  const signUpWithEmail = (email, password) => createUserWithEmailAndPassword(FirebaseAuth, email, password)
+
+  const signInWithEmail = (email, password) => signInWithEmailAndPassword(FirebaseAuth, email, password)
 
   return (
-    <FirebaseContext.Provider
-      value={{
-
-      }}>
+    <FirebaseContext.Provider value={{}}>
       {props.children}
     </FirebaseContext.Provider>
-  )
-}
+  );
+
+
+};
