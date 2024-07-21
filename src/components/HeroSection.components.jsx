23223981ef7e37e4490de0481261b435a4de36d2
Suryakant01 <suryakant160502@gmail.com>
2024-07-22 00:19:10 +0530

    updated github link in footer

--- src/components/HeroSection.components.jsx
+++ src/components/HeroSection.components.jsx
@@ -3,7 +3,7 @@ import { Button, Container } from 'react-bootstrap'
 import "../components/CSS/HeroSection.css"
 
 const HeroSection = () => {
-    const videoSource = "https://firebasestorage.googleapis.com/v0/b/bookify-59edc.appspot.com/o/Assests%2F3125396-uhd_3840_2160_25fps.mp4?alt=media&token=280f459a-5441-4577-a60d-dadc2d8d09a0";
+    const videoSource = "https://firebasestorage.googleapis.com/v0/b/safarnama-c075f.appspot.com/o/Assets%2FtravelVideo.mp4?alt=media&token=253087a6-9e38-4acb-ba81-74617f958810";
 
     return (
 
