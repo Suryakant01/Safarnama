a65383e387f424c90b13560fd36d01a9f48e7645
Suryakant01 <suryakant160502@gmail.com>
2024-08-06 02:55:52 +0530

    beautifed form comp

--- src/components/Form.components.jsx
+++ src/components/Form.components.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { useFirebase } from "../context/firebase.context.jsx";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faShare} from '@fortawesome/free-solid-svg-icons'
 import '../components/CSS/Form.css';
 
-const Form = ({ onSubmit }) => {
+const Form = () => {
     const firebase = useFirebase();
     const [name, setName] = useState('');
     const [place, setPlace] = useState('');
@@ -21,13 +23,13 @@ const Form = ({ onSubmit }) => {
     return (
         <div className='container'>
             <form className='blog-form' onSubmit={handleSubmit}>
-                <h2>
-                    Share your Experience for making others journey incredible
+                <h2 className='fw-bolder'>
+                    Share your Experience
                 </h2>
-                <input type='text' value={name} onChange={(e) => setName(e.target.value)} placeholder='Name' required />
-                <input type='text' value={place} onChange={(e) => setPlace(e.target.value)} placeholder='Place' required />
-                <input type='text' value={state} onChange={(e) => setState(e.target.value)} placeholder='State' required />
-                <textarea value={article} onChange={(e) => setArticle(e.target.value)} placeholder='Your Journey' required />
+                <input type='text' value={name} onChange={(e) => setName(e.target.value)} placeholder='How should people know you?' required />
+                <input type='text' value={place} onChange={(e) => setPlace(e.target.value)} placeholder='Beautiful Place You Travelled to..' required />
+                <input type='text' value={state} onChange={(e) => setState(e.target.value)} placeholder='State it falls in..' required />
+                <textarea value={article} onChange={(e) => setArticle(e.target.value)} placeholder='Your Amazing experience!!!' required />
                 
                 <input type='file' id='file' onChange={(e) => setDestPic(e.target.files[0])} style={{ display: 'none' }} />
                 <label htmlFor='file' className='custom-file-upload'>
@@ -35,7 +37,9 @@ const Form = ({ onSubmit }) => {
                 </label>
                 {destPic && <span className='file-name'>{destPic.name}</span>}
 
-                <button type='submit'>Submit Blog</button>
+                <button type='submit' className="share-button">Let the World Know 
+                <FontAwesomeIcon icon={faShare} className="share-icon" />
+                </button>
             </form>
         </div>
     );
