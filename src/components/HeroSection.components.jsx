98b1e8856b3aa879e12f44e444e09b5f4449d673
Suryakant01 <suryakant160502@gmail.com>
2024-08-23 01:36:52 +0530

    added navigation link

--- src/components/HeroSection.components.jsx
+++ src/components/HeroSection.components.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Button, Container } from 'react-bootstrap'
 import "../components/CSS/HeroSection.css"
 
 const HeroSection = () => {
     const videoSource = "https://firebasestorage.googleapis.com/v0/b/safarnama-c075f.appspot.com/o/Assets%2FtravelVideo.mp4?alt=media&token=253087a6-9e38-4acb-ba81-74617f958810";
+
+    const navigate = useNavigate();
 
     return (
 
@@ -15,7 +18,7 @@ const HeroSection = () => {
                 <Container className="text-center" style={{ position: "relative", zIndex: "1" }}>
                     <h1 className="hero-title" style={{ fontWeight: "bold" }}>Explore. Dream. Discover</h1>
                     <h4 className="hero-subtitle">Uncovering Hidden Gems Around the World</h4>
-                    <Button variant="outline-light" style={{  borderRadius: "20px" }}>Start Exploring &#8594;</Button>
+                    <Button onClick={() => navigate("/destination")} variant="outline-light" style={{  borderRadius: "20px" }}>Start Exploring &#8594;</Button>
                 </Container>
             </div>
         </div>
